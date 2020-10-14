5734335d088c641d04763de5434e241f0d0566c3
diegoncollazo <diego.nicolas@outlook.com.ar>
2020-10-13 21:20:33 -0300

    Seccion 27

---
+++ Netbeans/seccion_27/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ 
+jdbc:mysql://localhost:3306/blog?zeroDateTimeBehavior=CONVERT_TO_NULL [netbeans on Default schema]
+3jWp_q89#!88E]n */
--- Visual Code/seccion_27/create.sql
+++ Visual Code/seccion_27/create.sql
@@ -13,11 +13,25 @@ longtext
 */
 CREATE TABLE blog.usuarios
 (
-    id INT,
-    name varchar(100),
-    lastname varchar(100),
-    email varchar(100),
-    password varchar(100)
-)
-INSERT INTO `usuarios`(`id`, `name`, `lastname`, `email`, `password`) 
-VALUES (1,"Diego","Collazo","asd@asd.net","myPASS")
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name varchar(100)NOT NULL,
+    lastname varchar(100) DEFAULT 'sin apellido',
+    email varchar(100)NOT NULL,
+    password varchar(100),
+    PRIMARY KEY(id)
+);
+
+INSERT INTO usuarios(
+id, name, lastname, email, password) 
+VALUES
+(1,"Diego","Collazo","asd@asd.net","myPASS");
+
+-- Borrar tablas
+DROP TABLE usuarios;
+
+-- Modificar tablas
+-- Renombar tabla
+ALTER TABLE usuarios RENAME TO renombrados;
+-- Renombrar columnas
+ALTER TABLE renombrados CHANGE lastname apellidos VARCHAR(100) NULL;
+
