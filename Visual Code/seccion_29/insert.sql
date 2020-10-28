ffdcc8b68cc613ed08889873ead65f53ace5d90d
diegoncollazo <diego.nicolas@outlook.com.ar>
2020-10-28 00:44:50 -0300

    Seccion 30

---
+++ Visual Code/seccion_29/insert.sql
@@ -0,0 +1,10 @@
+-- Insert basico
+INSERT INTO usuarios VALUES
+(null, 'Diego', 'Collazo', 'diegoncollazo@gmail.com', 'zaq1XSW"', '1981-07-11');
+INSERT INTO usuarios VALUES
+(null, 'Lucía', 'Collazo', 'luciacollazo@gmail.com', 'zaq1XSW"', '2015-06-30');
+INSERT INTO usuarios VALUES
+(null, 'Juan Ignacio', 'Collazo', 'juanignaciocollazo@gmail.com', 'zaq1XSW"', '2015-07-29');
+
+-- INSERT avanzado
+INSERT INTO usuarios(email, password) VALUES('prueba@asd.net', 'asd');
---
+++ Visual Code/seccion_29/select.sql
@@ -0,0 +1,34 @@
+-- Mostrar todos los registros / filas de una tabla
+SELECT email, nombre, apellido FROM usuarios;
+
+-- Mostrar todos los campos
+SELECT * FROM usuarios;
+
+-- Operadores aritmeticos
+SELECT email, (7+7) AS 'Suma' FROM usuarios;
+SELECT email, (7-7) AS 'Suma' FROM usuarios;
+SELECT email, (7*7) AS 'Suma' FROM usuarios;
+SELECT email, (7/7) AS 'Suma' FROM usuarios;
+SELECT id, email, (7%7) AS 'Suma' FROM usuarios ORDER BY id DESC;
+SELECT id, email, (7%7) AS 'Suma' FROM usuarios ORDER BY id ASC;
+
+-- Operadores con texto
+SELECT CONCAT(nombre, ' ', apellido) AS CONVERSION FROM usuarios
+SELECT UPPER(CONCAT(nombre, ' ', apellido)) AS CONVERSION FROM usuarios
+SELECT LENGTH(CONCAT(nombre, ' ', apellido)) AS CONVERSION FROM usuarios
+SELECT TRIM(CONCAT('    ', nombre, ' ', apellido, '    ')) AS CONVERSION FROM usuarios
+
+-- Operadores con fechas
+SELECT email, fecha, CURDATE() AS 'Fecha actual' FROM usuarios
+SELECT DATEDIFF(CURDATE(), fecha) AS 'Diferencia de días' FROM usuarios
+SELECT email, DAYNAME(fecha) AS 'Día de la semana' FROM usuarios
+SELECT email, DAYOFMONTH(fecha) AS 'Día del mes' FROM usuarios
+SELECT email, DAYOFWEEK(fecha) AS 'Día de la semana' FROM usuarios
+SELECT email, DAYOFYEAR(fecha) AS 'Día del año' FROM usuarios
+SELECT email, DAY(fecha) AS 'Día del año' FROM usuarios
+SELECT email, MONTH(fecha) AS 'Día del año' FROM usuarios
+SELECT email, YEAR(fecha) AS 'Día del año' FROM usuarios
+SELECT email, HOUR(fecha) AS 'Día del año' FROM usuarios
+SELECT email, CURTIME() AS 'Hora actual' FROM usuarios
+SELECT email, SYSDATE() AS 'Hora del sistema' FROM usuarios
+SELECT email, DATE_FORMAT(fecha, '%d/%m/%Y') AS 'Fecha formateada' FROM usuarios
