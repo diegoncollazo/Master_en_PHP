c545b38988768291b6dd4bb3786f7807200a65e6
diegoncollazo <diego.nicolas@outlook.com.ar>
2021-01-06 01:13:03 -0300

    Seccion 33

--- seccion_33/agrupamiento.sql
+++ seccion_33/agrupamiento.sql
@@ -1,3 +1,4 @@
+-- Agrupamiento
 SELECT
     COUNT(titulo) AS 'Numero de entradas',
     categoria_id
@@ -5,3 +6,36 @@ FROM
     entradas
 GROUP BY
     categoria_id;
+
+-- Agrupamiento con condiciones
+SELECT
+    COUNT(titulo) AS 'Numero de entradas',
+    categoria_id
+FROM
+    entradas
+GROUP BY
+    categoria_id
+HAVING
+    COUNT(titulo) >= 4;
+
+-- Funciones 
+SELECT
+    AVG(id) AS 'Media id entradas'
+FROM
+    entradas;
+
+SELECT
+    MAX(id) AS 'Maximo id entradas',
+    titulo
+FROM
+    entradas;
+
+SELECT
+    MIN(id) AS 'Minimo id entradas'
+FROM
+    entradas;
+
+SELECT
+    SUM(id) AS 'Suma id entradas'
+FROM
+    entradas;
